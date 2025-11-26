33c8dfef2a48c8f95a9ea5ab6c0eb291224772b9
ShaneBeee <shanebolenback@me.com>
2025-11-26 11:34:42 -0800

    ExprChunkRegionalDifficulty - changed to use location instead of chunk - this doesn't really matter internally as it still checks just the chunk - but you never know what the future holds

--- src/main/java/com/shanebeestudios/nms/elements/expressions/ExprChunkRegionalDifficulty.java
+++ src/main/java/com/shanebeestudios/nms/elements/expressions/ExprChunkRegionalDifficulty.java
@@ -5,28 +5,34 @@ import ch.njol.skript.doc.Examples;
 import ch.njol.skript.doc.Name;
 import ch.njol.skript.doc.Since;
 import ch.njol.skript.expressions.base.SimplePropertyExpression;
-import com.shanebeestudios.nms.api.world.ChunkApi;
-import org.bukkit.Chunk;
+import com.mojang.datafixers.util.Pair;
+import com.shanebeestudios.nms.api.util.McUtils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import org.bukkit.Location;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 @Name("Chunk Local Difficulty")
-@Description({"Represents the local difficulty of a chunk.",
+@Description({"Represents the local difficulty of a location.",
     "This is the number that shows on the client's debug screen `Local Difficulty: (local difficulty) //...`",
     "Minecraft uses this to decide how monsters will spawn near players.",
-    "This number changes based on how long a player spends in a chunk as well as world time.",
+    "This number changes based on how long a player spends in a chunk, world time and moon phase.",
     "See [**Regional Difficulty on McWiki**](https://minecraft.wiki/w/Difficulty#Regional_difficulty) for more info."})
-@Examples("if local difficulty of chunk at player > 1:")
+@Examples("if local difficulty of location of player > 1:")
 @Since("1.0.0")
-public class ExprChunkRegionalDifficulty extends SimplePropertyExpression<Chunk, Number> {
+public class ExprChunkRegionalDifficulty extends SimplePropertyExpression<Location, Number> {
 
     static {
-        register(ExprChunkRegionalDifficulty.class, Number.class, "(local|regional) difficulty", "chunks");
+        register(ExprChunkRegionalDifficulty.class, Number.class, "(local|regional) difficulty", "locations");
     }
 
     @Override
-    public @Nullable Number convert(Chunk chunk) {
-        return ChunkApi.getEffectiveDifficulty(chunk);
+    public @Nullable Number convert(Location location) {
+        Pair<ServerLevel, BlockPos> levelPos = McUtils.getLevelPos(location);
+        ServerLevel level = levelPos.getFirst();
+        BlockPos pos = levelPos.getSecond();
+        return level.getCurrentDifficultyAt(pos).getEffectiveDifficulty();
     }
 
     @Override
