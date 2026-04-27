d25375cbe5e619b26e1072a6519c3f11f028cbfa
ShaneBeee <shanebolenback@me.com>
2026-04-27 16:03:07 -0700

    Add syntaxes to register tags

--- src/main/java/com/shanebeestudios/nms/elements/ElementRegistration.java
+++ src/main/java/com/shanebeestudios/nms/elements/ElementRegistration.java
@@ -7,6 +7,7 @@ import com.shanebeestudios.nms.elements.effects.EffApplyBiomeFeature;
 import com.shanebeestudios.nms.elements.effects.EffApplyBiomeSpawner;
 import com.shanebeestudios.nms.elements.effects.EffApplyBiomeTag;
 import com.shanebeestudios.nms.elements.effects.EffApplyEnvironmentalAttribute;
+import com.shanebeestudios.nms.elements.effects.EffApplyTag;
 import com.shanebeestudios.nms.elements.effects.EffBiomeFill;
 import com.shanebeestudios.nms.elements.effects.EffBlockFill;
 import com.shanebeestudios.nms.elements.effects.EffPlaceFeature;
@@ -21,6 +22,7 @@ import com.shanebeestudios.nms.elements.sections.SecBiomeRegister;
 import com.shanebeestudios.nms.elements.sections.SecBiomeSpawners;
 import com.shanebeestudios.nms.elements.sections.SecBiomeSpecialEffects;
 import com.shanebeestudios.nms.elements.sections.SecEnchantmentRegister;
+import com.shanebeestudios.nms.elements.sections.SecTagRegister;
 import com.shanebeestudios.nms.elements.structures.StructRegistryRegistration;
 import com.shanebeestudios.nms.elements.type.Types;
 
@@ -33,6 +35,7 @@ public class ElementRegistration {
         EffApplyBiomeSpawner.register(reg);
         EffApplyBiomeTag.register(reg);
         EffApplyEnvironmentalAttribute.register(reg);
+        EffApplyTag.register(reg);
         EffBiomeFill.register(reg);
         EffBlockFill.register(reg);
         EffPlaceFeature.register(reg);
@@ -53,6 +56,7 @@ public class ElementRegistration {
         SecBiomeSpecialEffects.register(reg);
         SecBiomeSpawners.register(reg);
         SecEnchantmentRegister.register(reg);
+        SecTagRegister.register(reg);
 
         // STRUCTURES
         StructRegistryRegistration.register(reg);
