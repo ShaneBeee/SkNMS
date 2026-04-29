434883aa4bb5164eea13472f056ae33752b9f79c
ShaneBeee <shanebolenback@me.com>
2026-04-29 07:54:18 -0700

    Update SKR again

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ dependencies {
     compileOnly("com.github.SkriptLang:Skript:2.14.1")
 
     // SkriptRegistration
-    implementation("com.github.shanebeee:SkriptRegistration:1.0.10")
+    implementation("com.github.ShaneBeee:SkriptRegistration:1.0.10")
 
     // SkBee
     compileOnly("com.github.ShaneBeee:SkBee:3.17.1@jar") // Forcing jar as it wasn't downloading
